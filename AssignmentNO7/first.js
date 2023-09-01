47c023a8f19c61da642b3341d9165f2928c7384d
Hamzagilltext <hamzagilltext@gmail.com>
2023-09-01 21:27:37 +0500

    first commit

---
+++ AssignmentNO7/first.js
@@ -0,0 +1,37 @@
+"use strict";
+// Write a program that calculates area of rectangle//
+let width = 59;
+let height = 40;
+let areaofrectangle = height * width;
+//console.log(`area of rectange is ${areaofrectangle}`);
+//Q2:write a program that takes input and calculates the volume of cube//
+let lengthofside = 4;
+let volumeofcube = lengthofside ** 3;
+//console.log(`volume of cube is ${volumeofcube}`);
+//Q3 checks if a given number is negative,positive or zero//
+let num = 1;
+if (num == 0) {
+    //console.log("the given number is zero");
+}
+else if (num > 0) {
+    //console.log("the given number is positive");
+}
+else {
+    //console.log("the given number is negative");
+}
+//Q4 check the number is even or odd
+let number = 37;
+if (number % 2 == 0) {
+    //console.log("the given number is even");
+}
+else {
+    //console.log("the given number is odd");
+}
+//Q5 check if the person is eligible for vote on age//
+let age = 27;
+if (age <= 25) {
+    console.log("you are able for voting");
+}
+else {
+    console.log("you are not able for voting");
+}
